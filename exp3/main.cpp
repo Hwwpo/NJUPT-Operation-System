a60e612b3adf4ba33889e9c5a6bc5792f341f69d
ppy <1422322819@qq.com>
2023-12-30 17:43:56 +0800

    完成了实验3注释

--- exp3/main.cpp
+++ exp3/main.cpp
@@ -6,18 +6,21 @@
 
 class Page {
 public:
-    int id;
-    int entryTime;
-    int lastUsedTime;
-    int futureLastUsedTime{};
+    int id;                     // 页面id
+    int entryTime;              // 进入内存时间
+    int lastUsedTime;           // 最后被使用时间
+    int futureLastUsedTime{};   // 未来第一次被使用时间
 
 public:
+    // 构造函数
     Page(int id, int entryTime, int lastUsedTime) : id(id), entryTime(entryTime), lastUsedTime(lastUsedTime) {
     }
+    // 两个页面只要id相等则他们相等
     bool operator==(const Page &other) const {
         return id == other.id;
     }
 };
+// 为Page编写hash函数
 namespace std {
     template<>
     struct hash<Page> {
@@ -28,36 +31,38 @@ namespace std {
     };
 }
 
-
+// 用户输入
 void input(int &blocks, int &referenceString, std::vector<int> &vector1);
-
+// 模拟置换步骤
 void pageReplaceAlgorithm(int &numberPhysicalBlocks, std::vector<int> &referenceString, int algorithm);
-
+// 向结果表中添加当前步骤的结果
 void addBlock(const std::vector<Page> &_blocks, std::vector<std::vector<char>> &table, int blocksNumber, bool isMissing);
-
+// OPT页面置换算法
 int OPT(std::vector<Page> &blocks, std::vector<int> &referenceString, int pos);
-
+// FIFO页面置换算法
+int FIFO(std::vector<Page> &_blocks);
+// LRU页面置换算法
+int LRU(std::vector<Page> &_blocks);
+// 打印结果表
 void printTable(const std::vector<std::vector<char>> &table, const std::vector<int> &referenceString);
 
-int FIFO(std::vector<Page> &_blocks);
-
-int LRU(std::vector<Page> &_blocks);
-
 int main() {
-    int numberPhysicalBlocks;
-    int lengthPageNumberReferenceString;
-    std::vector<int> pageNumberReferenceString;
+    int numberPhysicalBlocks;   // 物理块数量
+    int lengthPageNumberReferenceString;    // 页面引用串长度
+    std::vector<int> pageNumberReferenceString; // 页面引用串内容
     input(numberPhysicalBlocks, lengthPageNumberReferenceString, pageNumberReferenceString);
     for(int i = 1; i < 4; i++)
+        // 分别调用不同算法
         pageReplaceAlgorithm(numberPhysicalBlocks, pageNumberReferenceString, i);
     return 0;
 }
 
 void pageReplaceAlgorithm(int &numberPhysicalBlocks, std::vector<int> &referenceString, int algorithm) {
-    int count = 1;
-    int missCount = 0;
-    std::vector<Page> blocks;
-    std::vector<std::vector<char>> table(numberPhysicalBlocks + 1);
+    int count = 1;  // 步骤计数器
+    int missCount = 0;  // 缺页计数器
+    std::vector<Page> blocks;   // 物理块模拟
+    std::vector<std::vector<char>> table(numberPhysicalBlocks + 1); // 结果表
+    // 打印算法标题
     switch (algorithm) {
         case 1:
             std::cout << "OPT算法：" << std::endl;
@@ -71,7 +76,9 @@ void pageReplaceAlgorithm(int &numberPhysicalBlocks, std::vector<int> &reference
         default:
             break;
     }
+    // 开始模拟
     for(int i = 0; i < referenceString.size(); i++, count++) {
+        // 进入一个新页面
         int pageId = referenceString[i];
         bool isExist = false;
 
@@ -95,7 +102,9 @@ void pageReplaceAlgorithm(int &numberPhysicalBlocks, std::vector<int> &reference
             addBlock(blocks, table, numberPhysicalBlocks, true);
             continue;
         }
+        // 如果不存在且已填满
         int replacePos;
+        // 调用算法获取置换位置
         switch (algorithm) {
             case 1:
                 replacePos = OPT(blocks, referenceString, i + 1);
@@ -110,18 +119,18 @@ void pageReplaceAlgorithm(int &numberPhysicalBlocks, std::vector<int> &reference
                 replacePos = 0;
                 break;
         }
-//        int replacePos = OPT(blocks, referenceString, i + 1);
-//        int replacePos = FIFO(blocks);
         Page page(pageId, count, count);
         blocks[replacePos] = page;
         missCount++;
         addBlock(blocks, table, numberPhysicalBlocks, true);
     }
+    // 打印结果
     printTable(table, referenceString);
     std::cout << "缺页次数:" << missCount << "  缺页率:" << missCount * 1.0 / referenceString.size() << std::endl;
 }
 
 int LRU(std::vector<Page> &_blocks) {
+    // 寻找最小使用时间即可
     auto min_it = std::min_element(_blocks.begin(), _blocks.end(),
                                    [](const Page &A, const Page &B) {
                                        return A.lastUsedTime < B.lastUsedTime;
@@ -130,6 +139,7 @@ int LRU(std::vector<Page> &_blocks) {
 }
 
 int FIFO(std::vector<Page> &_blocks) {
+    // 寻找最小的进入时间
     auto min_it = std::min_element(_blocks.begin(), _blocks.end(),
                                    [](const Page &A, const Page &B) {
                                        return A.entryTime < B.entryTime;
@@ -139,9 +149,11 @@ int FIFO(std::vector<Page> &_blocks) {
 
 void printTable(const std::vector<std::vector<char>> &table, const std::vector<int> &referenceString) {
     int cell_width = 3;
+    // 打印表头
     for(auto item:referenceString)
         std::cout << std::setw(cell_width) << item;
     std::cout << std::endl;
+    // 打印内容
     for(const auto& row:table) {
         for (auto item: row)
             std::cout << std::setw(cell_width) << item;
@@ -161,15 +173,18 @@ int OPT(std::vector<Page> &blocks, std::vector<int> &referenceString, int pos) {
         if(it != blocks.end()) {
             Page &page = *it;
             if(!flag[page]){
+                // 只找第一次使用的
                 page.futureLastUsedTime = i;
                 flag[page] = true;
             }
         }
     }
+    // 如果以后再也没有使用，就假设在引用串最后一项结束后使用
     for(auto &item:blocks) {
         if(!flag[item])
             item.futureLastUsedTime = int(referenceString.size());
     }
+    // 寻找最大的第一次使用时间
     auto max_it = std::max_element(blocks.begin(), blocks.end(),
                                    [](const Page &A, const Page &B) {
         return A.futureLastUsedTime < B.futureLastUsedTime;
@@ -180,6 +195,7 @@ int OPT(std::vector<Page> &blocks, std::vector<int> &referenceString, int pos) {
 
 void addBlock(const std::vector<Page> &_blocks, std::vector<std::vector<char>> &table, int blocksNumber, bool isMissing) {
     int i = 0;
+    // 缺页标记
     if(isMissing)
         table[i].push_back('*');
     else
